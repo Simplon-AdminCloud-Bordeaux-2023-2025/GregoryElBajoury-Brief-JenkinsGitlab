df8c03ddd0c415b42621e04d84d4544ddbd855f2
Gregory ELBAJOURY <34866703+Injechta@users.noreply.github.com>
2024-02-08 11:56:09 +0100

    Update script.sh :  Bash install gitlab script

--- gitlab/script.sh
+++ gitlab/script.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+# Mise à jour des paquets
+sudo apt-get update -y
+
+# Installation des dépendances
+sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
+
+# Téléchargement du package GitLab
+curl -LO https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh
+
+# Exécution du script d'installation de GitLab
+sudo bash script.deb.sh
+
+# Installation de GitLab Community Edition
+sudo apt-get install -y gitlab-ce
+
+# Démarrage des services GitLab
+sudo gitlab-ctl reconfigure
 
